3d1a5898dd80967c3670b301f9f2e3c32c921305
Michal Toger <michaltoger@gmail.com>
2022-12-25 15:57:42 +0200

    Exercise 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Counter from './Counter';
+import { useState } from 'react';
 
 function App() {
+  const [delda, setDelta] = useState(1)
+  function handleDelta (e){
+    console.log(e);
+    setDelta(Number(e.target.value))
+  }
   return (
     <div className="App">
       Usestate
-      <Counter/>
+      <input type= "number" value= {delda} onChange = {handleDelta}/>
+      <Counter delda = {delda}/>
+      <Counter delda = {delda}/>
     </div>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,26 +1,34 @@
 import { useState } from "react"; 
 
-function Counter(){
+function Counter(props){
+    const {delda} = props
     const [count, setCount] = useState(1)
-    const [delda, setDelta] = useState(1)
+
+   
 
     function incr(){
         setCount(
             function(oldCount){
-               return oldCount + 1; 
+               return oldCount + delda; 
             }
         )
-        console.log(count)
     }
+   function reset(){
+        setCount(0)
+   }
+  
+   
+
     return (
       <div className="App">
         <h1>Counter</h1>
-        <input type= "number" value= {delda}/>
         <p>Counter is at {count}</p>
-        <button onClick={incr}>Click to add 1 to Counter</button>
+        <button onClick={incr}>Click to add {delda} to Counter</button>
         <button onClick={reset}>Reset Counter</button>
       </div>
     );
-  }
+    }
+
+  
   
   export default Counter;
